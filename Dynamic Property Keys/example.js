442626aa622012e2f3b686d1784de6020a89d96d
RondinelliNelvam <rondi135@gmail.com>
2022-09-16 16:32:12 -0300

    Dynamic Property Keys Att Example

---
+++ Dynamic Property Keys/example.js
@@ -0,0 +1,37 @@
+const { branchExample } = require("../utils/utils");
+const { dbExample } = require("../utils/dbExample");
+object1 = [
+  {
+    positivo: 0,
+    negativo: 0,
+  },
+  {
+    algo: 0,
+    sim: 0,
+  },
+];
+object2 = ["a"];
+// let key = object2[0].teste;
+
+let algo = dbExample.map((item, index) => {
+  for (let i = 0; i < branchExample.length; i++) {
+    let key = branchExample[index].branchName;
+
+    const calculation = item.allHistoric.reduce(
+      (acumulator, currentValue) => {
+        acumulator[`totalValueFrom${key}`] += currentValue.quantity;
+        return acumulator;
+      },
+      { [`totalValueFrom${key}`]: 0 }
+    );
+    return { ...dbExample, ...calculation };
+  }
+});
+console.log(Object.values({ ...algo, ...object2 }));
+console.log("---");
+console.log(Object.values(algo));
+
+// console.log({ ...algo, ...object2 });
+
+// console.log(algo);
+// console.log(dbExample[0].allHistoric);
---
+++ Dynamic Property Keys/index.js
@@ -0,0 +1,9 @@
+let key = "awt";
+const age = "idade";
+
+const me = {
+  [key]: "AAA",
+  [age]: "11",
+};
+
+console.log(me);
