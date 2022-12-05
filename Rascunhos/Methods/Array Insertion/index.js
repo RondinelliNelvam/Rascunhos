6b8ad287095e609189a859ed295874bd833e17ca
RondinelliNelvam <rondi135@gmail.com>
2022-12-05 08:36:46 -0300

    Array Insertion

--- Rascunhos/Methods/Array Insertion/index.js
+++ Rascunhos/Methods/Array Insertion/index.js
@@ -24,6 +24,7 @@ if (method == 1) {
 }
 // let [a, b, ...args] = [1, 2, 3, 4, 5];
 // console.log(args);
+let arrayKey = ["teste", "teste3", "teste2"];
 let newArrayTwo = [];
 let array3 = [
   [{ teste: [1, 2] }, { teste3: [2, 3] }, { teste2: [3, 4] }],
@@ -31,15 +32,27 @@ let array3 = [
   [{ teste: [3, 4] }, { teste3: [4, 5] }, { teste2: [5, 6] }],
   [{ teste: [4, 5] }, { teste3: [5, 6] }, { teste2: [1, 2] }],
 ];
+
+newArrayTwo = array3[0].map((element) => element);
+// console.log(newArrayTwo.length);
+// console.log(array3.length);
 for (let i = 0, j = 1; j < array3.length; i++, j++) {
-  for (let a = 0; a < array3[i].length; a++) {
-    if (i === 0) {
-      newArrayTwo.push([array3[i][a]]);
-      console.log(newArrayTwo);
-    } else {
-      // newArrayTwo[a].push(Object.values(array3[i][a]));
-    }
+  // console.log(newArrayTwo);
+  for (let a = 0; a < newArrayTwo.length; a++) {
+    newArrayTwo[a][Object.keys(newArrayTwo[a])[0]].push(
+      ...array3[j][a][Object.keys(array3[j][a])[0]]
+    );
   }
 }
-array3[0][0][Object.keys(array3[0][0])[0]];
-console.log();
+console.log(Object.keys(newArrayTwo[0]));
+// newArrayTwo[0].teste.push(...[3, 4]);
+console.log(newArrayTwo);
+// array3[0][0][Object.keys(array3[0][0])[0]];
+// const algo = Object.assign(array3[0][0].teste, array3[1][0].teste);
+// console.log(array3[0][0][Object.keys(array3[0][0])[0]]);
+// console.log(Object.keys(array3[0][0])[0]);
+// console.log(algo);
+// console.log(array3[0][0].teste);
+// array3[i][a][Object.keys(array3[i][a])[0]].push(
+//   ...array3[j][a][Object.keys(array3[j][a])[0]]
+// );
